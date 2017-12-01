0d3af2163ef34af646ee02bfdf7cb255fedab3e4
Pedro Augusto <pedro.resende99@gmail.com>
2017-11-30 22:55:29 -0200

    30-11-2017

--- Data Structure/stack.c
+++ Data Structure/stack.c
@@ -4,10 +4,10 @@ typedef struct {
 
 typedef struct node_t {
 	struct node_t *next;
-	data_t data;	
+	data_t data;
 } node_t;
 
-typedef node_t stack;
+typedef node_t stack_t;
 
 /*
 	Push a data into a dynamic stack.
@@ -16,24 +16,24 @@ typedef node_t stack;
 	param data_t data - data to be inserted into the refered stack
 	return: in success, return 1; in failure, return 0
 */
-int push(node_t **stack, data_t data) {
+int push(stack_t **stack, data_t data) {
 	node_t *new_node = malloc(sizeof(node_t));
-	
+
 	if (new_node == NULL)
 		return 0;
-		
+
 	new_node->data = data;
 	new_node->next = (*stack);
 	(*stack) = new_node;
 	return 1;
 }
 
-int pop(node_t **stack) {
+int pop(stack_t **stack) {
 	node_t *p;
-	
+
 	if (p == NULL)
 		return 0;
-	
+
 	p = *stack;
 	*stack = (*stack)->next;
 	free(p);
