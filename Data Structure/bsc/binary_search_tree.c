5490b052512faafd72ca4fd0a925f49ea074c6bd
Pedro Augusto <pedro.resende99@gmail.com>
2017-12-15 13:17:04 -0200

    Update BST

--- Data Structure/bsc/binary_search_tree.c
+++ Data Structure/bsc/binary_search_tree.c
@@ -1,29 +1,33 @@
-typedef struct {
-	int id;
-} data_t;
-
 typedef struct node_t {
 	int key;
 	struct node_t *lchild;
 	struct node_t *rchild;
 } node_t;
 
-int obtainLevel(node_t *tree,  int num) {
-	int level = 0;
+void insert(node_t **root, int key) {
+	static node_t *new_node = NULL;
 
-	while (tree && tree->key != num) {
-		if (num < tree->key)
-			tree = tree->lchild;
-		else
-			tree = tree->rchild;
+	new_node = malloc(sizeof(node_t));
+	new_node->lchild = new_node->rchild = NULL;
+	new_node->key = key;
 
-		level++;
+	if ((*root) == NULL) {
+		(*root) = new_node;
+	} else if (new_node->key < (*root)->key) {
+		insert(&(*root)->lchild, key);
+	} else {
+		insert(&(*root)->rchild, key);
 	}
+}
 
-	if (!tree)
-		level = -1;
-
-	return level;
+node_t* search(node_t *root, int key) {
+	if (root == NULL || root->key == key) {
+		return root;
+	} else if (root->key > key) {
+		return search(root->lchild, key);
+	} else {
+    return search(root->rchild, key);
+	}
 }
 
 node_t* search_parent(node_t *tree,  int num) {
@@ -42,6 +46,53 @@ node_t* search_parent(node_t *tree,  int num) {
 		parent = NULL;
 
 	return parent;
+}
+
+void remove(node_t *tree, int num) {
+	node_t *target, *parent, *substitute;
+
+	target = search(tree, num);
+
+	if (!target) return;
+
+	parent = search_parent(tree, num);
+
+	if (!target->lchild)
+		substitute = target->rchild;
+	else {
+
+	}
+}
+
+void clear_tree(node_t *root) {
+	if (root != NULL) {
+		clear_tree(root->lchild);
+		clear_tree(root->rchild);
+		free(root);
+	}
+}
+
+/*
+	Some extra functions
+*/
+
+/*
+int obtainLevel(node_t *tree,  int num) {
+	int level = 0;
+
+	while (tree && tree->key != num) {
+		if (num < tree->key)
+			tree = tree->lchild;
+		else
+			tree = tree->rchild;
+
+		level++;
+	}
+
+	if (!tree)
+		level = -1;
+
+	return level;
 }
 
 int childs(node_t *tree) {
@@ -75,53 +126,4 @@ int height(node_t *root) {
    }
 }
 
-void clear_tree(node_t *root) {
-	if (root != NULL) {
-		clear_tree(root->lchild);
-		clear_tree(root->rchild);
-		free(root);
-	}
-}
-
-void insert(node_t **root, int key) {
-	static node_t *new_node = NULL;
-
-	new_node = malloc(sizeof(node_t));
-	new_node->lchild = new_node->rchild = NULL;
-	new_node->key = key;
-
-	// Insercao de inicio
-	if ((*root) == NULL) {
-		(*root) = new_node;
-	} else if (new_node->key < (*root)->key) {
-		insert(&(*root)->lchild, key);
-	} else {
-		insert(&(*root)->rchild, key);
-	}
-}
-
-node_t* search(node_t *root, int key) {
-	if (root == NULL || root->key == key) {
-		return root;
-	} else if (root->key > key) {
-		return search(root->lchild, key);
-	} else {
-    return search(root->rchild, key);
-	}
-}
-
-void remove(node_t *tree, int num) {
-	node_t *target, *parent, *substitute;
-
-	target = search(tree, num);
-
-	if (!target) return;
-
-	parent = search_parent(tree, num);
-
-	if (!target->lchild)
-		substitute = target->rchild;
-	else {
-
-	}
-}
+*/
